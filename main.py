3854a079e074bb93068066ff42ef3873a65189a9
gmoissey <gmoisseyev2@huskers.unl.edu>
2021-01-26 13:58:23 -0600

    changed RSS parsing

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from datetime import timedelta, datetime
 from dateutil import parser
 from pprint import pprint
 from time import sleep
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
 import requests
 import feedparser
 import config
@@ -18,24 +21,39 @@ def main():
     rss_feed = feedparser.parse(FEED_URL)
 
     for entry in rss_feed.entries:
-        parsed_date = parser.parse(entry.published)
-        parsed_date = (parsed_date - timedelta(hours=8)).replace(tzinfo=None) # remove timezone offset
-        now_date = datetime.utcnow()
 
-        published_30_minutes_ago = now_date - parsed_date < timedelta(minutes=30)
+        parsed_date = parser.parse(entry.published)
+        parsed_date = (parsed_date - timedelta(hours=0)).replace(tzinfo=None) # remove timezone offset
+        now_date = datetime.utcnow()
+        
+        
+        published_30_minutes_ago = now_date - parsed_date < timedelta(minutes=(60 * 24 * 8))
+
+
         if (published_30_minutes_ago):
+            
+            photoURL = title = entry.link
+
+            try:
+                html = urlopen(entry.link)
+                bs = BeautifulSoup(html, 'html.parser')
+                images = bs.find_all('img', {'src':re.compile('.jpg')})
+                photoURL = images[0]['src']
+            except:
+                pass
+    
 
             title = entry.title
             summary = entry.summary
-            link = entry.links[0]["href"]
-            photoUrl = entry.links[1]["href"].split("php?url=", 1)[1]
+            link = entry.link
+            #photoUrl = entry.links[1]["href"].split("php?url=", 1)[1]
 
-            content = ("<b>%s\n</b>%s\n<a href=\"%s\">|</a><a href=\"%s\">Continue Reading...</a>" % (title, summary, photoUrl,link))
+            content = ("<b>%s\n</b>%s\n<a href=\"%s\">Continue Reading..</a><a href=\"%s\">.</a>" % (title, summary, link, photoURL))
             send_message(content)
-        
+
 
 
 if __name__ == "__main__":
     while(True):
         main()
-        sleep(30 * 60)
+        sleep(60 * 60 * 24 * 8)
